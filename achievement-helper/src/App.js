f7b6feb5792a9dbd99809e247c12d61edde2a174
Sam Greaves <samgreaves0@gmail.com>
2019-04-02 18:15:28 +0100

    AH-5 Add xbox profile name

--- achievement-helper/src/App.js
+++ achievement-helper/src/App.js
@@ -3,16 +3,35 @@ import './App.css';
 import {Header} from "./Components/Header/Header.js"
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header/>
-          <body>
-          hi
-          </body>
-      </div>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {
+            xboxProfile: {}
+        };
+    }
+
+    componentDidMount() {
+        this.getXboxProfile();
+    }
+
+    getXboxProfile() {
+      fetch('https://xboxapi.com/v2/xxxx/profile', {
+        method: 'get',
+          headers: new Headers({
+              'X-AUTH': 'xxxx'
+          })
+      }).then(res => res.json()).then(json => {this.setState({xboxProfile: json})});
+    }
+
+    render() {
+      return (
+        <div className="App">
+          <Header xboxProfile={this.state.xboxProfile}/>
+            <body>
+            </body>
+        </div>
+      );
+    }
 }
 
 export default App;
