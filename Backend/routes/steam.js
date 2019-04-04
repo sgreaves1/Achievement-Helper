021b2c354edbc120de3439039c128958ee027943
Sam Greaves <samgreaves0@gmail.com>
2019-04-04 13:00:38 +0100

    AH-13 moved steam api specific code out of steam routes file

--- Backend/routes/steam.js
+++ Backend/routes/steam.js
@@ -1,26 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const nconf = require('nconf');
 const HttpStatus = require('literal-http-status');
 const steamAPI = require('../data/steamAPI');
-const rp = require('request-promise');
-
-let url = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=xxxx&steamids=xxxx';
 
 router.get('/profile', async function (request, response) {
-
-    let playerSummary = await rp(url);
-
-    let profiles = JSON.parse(playerSummary)['response']['players'];
-
-    if (profiles.length > 0)
-        response.status(HttpStatus['OK']).send(profiles[0]);
-    else
-        response.status(HttpStatus['Internal Server Error']).send(error);
+    response.status(HttpStatus['OK']).send(await steamAPI.getSteamProfile());
 });
-
-
-
-
 
 module.exports = router;
