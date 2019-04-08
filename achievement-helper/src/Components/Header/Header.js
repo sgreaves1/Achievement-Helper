5862ebc4f9135fea8c87548ca79cedcc7071fe16
Sam Greaves <samgreaves0@gmail.com>
2019-04-08 07:56:52 +0100

    AH-24 remove react header and add gamer pic and score

--- achievement-helper/src/Components/Header/Header.js
+++ achievement-helper/src/Components/Header/Header.js
@@ -1,6 +1,5 @@
 import React from "react";
 import './Header.css'
-import logo from './logo.svg';
 import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
 import Carousel from "react-bootstrap-carousel";
 import "bootstrap/dist/css/bootstrap.css";
@@ -16,69 +15,32 @@ export class Header extends React.Component {
     render() {
         return(
             <div>
+                <Carousel
+                animation="true"
+                autoplay="true"
+                slideshowSpeed="5000"
+                version="4">
+                    <div className="profile-slide">
+                        <span>Sam Greaves</span>
+                        <div className="carousel-caption">Sam Greaves</div>
 
-            <Carousel
-            animation="true"
-            autoplay="true"
-            slideshowSpeed="5000"
-            version="4">
-                <div className="profile-slide">
-                    <span>Sam Greaves</span>
-                    <div className="carousel-caption">Sam Greaves</div>
+                    </div>
+                    <div class="xbox-slide">
+                            <div class="xbox-gamercard">
+                                <img src={this.props.xboxProfile.GameDisplayPicRaw}></img>
+                                {this.props.xboxProfile.Gamertag}
+                                {this.props.xboxProfile.Gamerscore}
+                            </div>
+                            <div className="placeHolder">hello</div>
+                    </div>
+                    <div class="col-sm steam-slide" >
+                        <span>
+                            <text>{this.props.steamProfile.personaname}</text>
+                        </span>
+                    </div>
+                </Carousel>
 
-                </div>
-                <div className="xbox-slide">
-                    <span>{this.props.xboxProfile.Gamertag}</span>
-                </div>
-                <div className="profile-slide">
-                    <span>{this.props.steamProfile.personaname}</span>
-                </div>
-                {/*<Carousel.Item>*/}
-                    {/*<img*/}
-                    {/*className="d-block w-100"*/}
-                    {/*src="holder.js/800x400?text=Sam Greaves&bg=373940"*/}
-                    {/*alt="Profile Slide"/>*/}
-                    {/*<Carousel.Caption>*/}
-                        {/*<h3>First slide label</h3>*/}
-                        {/*<p>some text here</p>*/}
-                    {/*</Carousel.Caption>*/}
-                {/*</Carousel.Item>*/}
-                {/*<Carousel.Item>*/}
-                    {/*<img*/}
-                        {/*className="d-block w-100"*/}
-                        {/*src="holder.js/800x400?text=Sam Greaves&bg=373940"*/}
-                        {/*alt="Xbox Slide"/>*/}
-                    {/*<Carousel.Caption>*/}
-                        {/*<h3>First slide label</h3>*/}
-                        {/*<p>some text here</p>*/}
-                    {/*</Carousel.Caption>*/}
-                {/*</Carousel.Item>*/}
-                {/*<Carousel.Item>*/}
-                    {/*<img*/}
-                        {/*className="d-block w-100"*/}
-                        {/*src="holder.js/800x400?text=Sam Greaves&bg=373940"*/}
-                        {/*alt="Steam Slide"/>*/}
-                    {/*<Carousel.Caption>*/}
-                        {/*<h3>First slide label</h3>*/}
-                        {/*<p>some text here</p>*/}
-                    {/*</Carousel.Caption>*/}
-                {/*</Carousel.Item>*/}
-            </Carousel>
 
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
             </div>
         )
     }
