46dc2dffa59cbbce2cf7f88674bb81dffb851a34
Sam Greaves <samgreaves0@gmail.com>
2019-04-09 08:14:32 +0100

    AH-28 slightly improve xbox slide component

--- achievement-helper/src/Components/XboxProfileSlide/XboxProfileSlide.js
+++ achievement-helper/src/Components/XboxProfileSlide/XboxProfileSlide.js
@@ -5,20 +5,20 @@ export class XboxProfileSlide extends React.Component {
     render() {
         return(
             <div class="xbox-slide2 container-fluid">
-                <div class="row1">
-                    <div class="column1"/>
-                    <div class="column2"/>
-                    <div class="column3"/>
+                <div class="row-1">
+                    <div class="column-1"/>
+                    <div class="column-2"/>
+                    <div class="column-3"/>
                 </div>
-                <div class="row2">
-                    <div class="column1"/>
-                    <div class="column2"/>
-                    <div class="column3"/>
+                <div class="row-2">
+                    <div class="column-1"/>
+                    <div class="column-2"/>
+                    <div class="column-3"/>
                 </div>
-                <div class="row3">
-                    <div class="column1"/>
-                    <div class="column2"/>
-                    <div class="column3"/>
+                <div class="row-3">
+                    <div class="column-1"/>
+                    <div class="column-2"/>
+                    <div class="column-3"/>
                 </div>
             </div>
         )
